a0de75ba11ef65e5405992c49d8b849eb4bebfa3
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-11-06 02:00:35 +0600

    review sectiona added

---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.Init";
+import ViewReview from "../viewReview/ViewReview";
+
+const Review = () => {
+  const [user] = useAuthState(auth);
+  const [reviews, setReviews] = useState([]);
+  const [textField, setDescription] = useState("");
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const navigate = useNavigate();
+
+  const name = user?.displayName;
+  const img = "https://i.ibb.co/M8pgDX9/1024px-User-avatar-svg.png";
+  const time = Date().toLocaleString();
+  const description = textField;
+
+  useEffect(() => {
+    fetch("https://warehouse-management-2z04.onrender.com/reviewCount")
+      .then((res) => res.json())
+      .then((data) => setPageCount(Math.ceil(data.count / 3)));
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://warehouse-management-2z04.onrender.com/reviews?page=${page}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, [page]);
+
+  const handleComment = () => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      const review = { name, img, time, description };
+      fetch("https://warehouse-management-2z04.onrender.com/review", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(review),
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          const newReview = [...reviews, review];
+          setReviews(newReview);
+          alert("Comment successfully");
+        });
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="my-5">
+        <hr />
+        <hr />
+        <h4>Add Your Comment!!</h4>
+        <textarea
+          onBlur={(e) => setDescription(e.target.value)}
+          id=""
+          rows="5"
+          style={{ width: "75%" }}
+        ></textarea>
+        <br />
+        <button onClick={handleComment} className="btn btn-primary ms-2">
+          Post Comment
+        </button>
+        <hr />
+        <hr />
+      </div>
+      <div>
+        {reviews.map((review) => (
+          <ViewReview key={review._id} review={review} />
+        ))}
+      </div>
+      <div className="d-flex justify-content-center">
+        {[...Array(pageCount).keys()].map((number, i) => (
+          <button
+            key={i}
+            onClick={() => setPage(number)}
+            className={`me-2 ${
+              page === number ? "bg-primary text-light" : "bg-light"
+            }`}
+          >
+            {number + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Review;
